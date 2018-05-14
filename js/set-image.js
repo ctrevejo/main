752c106ec771d14191b76062f9fa8d497d74e5c4
ctrevejo <39265554+ctrevejo@users.noreply.github.com>
2018-05-15 00:09:29 +0200

    Primera Carga

---
+++ js/set-image.js
@@ -0,0 +1,77 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+	target2: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    this.setupFadeAnimation();
+
+    el.addEventListener(data.on, function () {
+      // Fade out image.
+      data.target.emit('set-image-fade');
+      // Wait for fade to complete.
+      setTimeout(function () {
+        // Set image.
+		  data.target.setAttribute('material', 'src', data.src);
+		  data.target.setAttribute('visible', true);
+		  data.target2.setAttribute('visible', true);
+		  document.getElementById("cursor").setAttribute('raycaster', 'objects: .closeButton');
+      }, data.dur);
+    });
+  },
+
+  /**
+   * Setup fade-in + fade-out.
+   */
+  setupFadeAnimation: function () {
+    var data = this.data;
+    var targetEl = this.data.target;
+
+    // Only set up once.
+    if (targetEl.dataset.setImageFadeSetup) { return; }
+    targetEl.dataset.setImageFadeSetup = true;
+
+    // Create animation.
+    targetEl.setAttribute('animation__fade', {
+      property: 'material.color',
+      startEvents: 'set-image-fade',
+      dir: 'alternate',
+      dur: data.dur,
+      from: '#FFF',
+      to: '#000'
+    });
+  }
+});
+
+
+AFRAME.registerComponent('close-image', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+	  
+    el.addEventListener(data.on, function () {
+    	el.setAttribute('visible', false);
+		data.target.setAttribute('material', 'src', '');
+		data.target.setAttribute('visible', false);
+		document.getElementById("cursor").setAttribute('raycaster', 'objects: .link');
+    });
+  }
+});
